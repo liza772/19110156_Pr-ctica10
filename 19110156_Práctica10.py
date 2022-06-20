26fb79f738595392c14a79b60d5fbac6dc76d237
liza772 <93304642+liza772@users.noreply.github.com>
2022-06-19 21:26:05 -0500

    main

--- 19110156_Práctica10.py
+++ 19110156_Práctica10.py
@@ -1,3 +1,9 @@
+
+#19110156
+#Objetivo: De la imagen que deseen separar por medio de un ROI 
+#el fondo de la imagen dejando únicamente el ROI al cual se 
+#le buscarán todas las esquinas.
+
 import cv2
 import numpy as np
 
